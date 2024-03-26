ce29f000d49782772c50973efcb88b51234bda9f
Veronica Parra <vero.parragraca@gmail.com>
2024-03-26 20:21:11 +0100

    configured

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-let firebaseConfig = { //objeto de configuración de Firebase
+const firebaseConfig = {
+  // Your web app's Firebase configuration
+  apiKey: "AIzaSyDsQ9Dbu9-YL8rhsLKsbVwdrCYyqNah8uQ",
+  authDomain: "fir-firebase-789bc.firebaseapp.com",
+  projectId: "fir-firebase-789bc",
+  storageBucket: "fir-firebase-789bc.appspot.com",
+  messagingSenderId: "364650381243",
+  appId: "1:364650381243:web:85c736d3cca1c1ac0a0e9e",
 };
 
-firebase.initializeApp(firebaseConfig);// Inicializaar app Firebase
+firebase.initializeApp(firebaseConfig); // Inicializaar app Firebase
 
-const db = firebase.firestore();// db representa mi BBDD //inicia Firestore
+const db = firebase.firestore(); // db representa mi BBDD //inicia Firestore
 
 //Función auxiliar para pintar una foto en el album
 const printPhoto = (title, url, docId) => {
-  let card = document.createElement('article');
-  card.setAttribute('class', 'card');
-  let picture = document.createElement('img');
-  picture.setAttribute('src', url);
-  picture.setAttribute('style', 'max-width:250px');
-  let caption = document.createElement('p');
+  let card = document.createElement("article");
+  card.setAttribute("class", "card");
+  let picture = document.createElement("img");
+  picture.setAttribute("src", url);
+  picture.setAttribute("style", "max-width:250px");
+  let caption = document.createElement("p");
   caption.innerHTML = title;
-  let id = document.createElement('p');
+  let id = document.createElement("p");
   id.innerHTML = docId;
-  const album = document.getElementById('album');
+  const album = document.getElementById("album");
   card.appendChild(picture);
   card.appendChild(caption);
   card.appendChild(id);
@@ -28,7 +35,7 @@ const createPicture = (picture) => {
   db.collection("album")
     .add(picture)
     .then((docRef) => {
-      console.log("Document written with ID: ", docRef.id)
+      console.log("Document written with ID: ", docRef.id);
       readAll();
     })
     .catch((error) => console.error("Error adding document: ", error));
@@ -44,29 +51,31 @@ const readAll = () => {
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        printPhoto(doc.data().title, doc.data().url, doc.id)
+        printPhoto(doc.data().title, doc.data().url, doc.id);
       });
-
     })
-    .catch(() => console.log('Error reading documents'));;
+    .catch(() => console.log("Error reading documents"));
 };
 
 //Delete
 const deletePicture = () => {
-  const id = prompt('Introduce el ID a borrar');
-  db.collection('album').doc(id).delete().then(() => {
-    alert(`Documento ${id} ha sido borrado`);
-    //Clean
-    document.getElementById('album').innerHTML = "";
-    //Read all again
-    readAll();
-  })
-    .catch(() => console.log('Error borrando documento'));
+  const id = prompt("Introduce el ID a borrar");
+  db.collection("album")
+    .doc(id)
+    .delete()
+    .then(() => {
+      alert(`Documento ${id} ha sido borrado`);
+      //Clean
+      document.getElementById("album").innerHTML = "";
+      //Read all again
+      readAll();
+    })
+    .catch(() => console.log("Error borrando documento"));
 };
 
-//Clean 
+//Clean
 const cleanAlbum = () => {
-  document.getElementById('album').innerHTML = "";
+  document.getElementById("album").innerHTML = "";
 };
 
 //Show on page load
@@ -80,7 +89,7 @@ document.getElementById("create").addEventListener("click", () => {
   const url = prompt("Introduce la url de tu foto");
   if (!title || !url) {
     alert("Hay un campo vacio. No se ha salvado");
-    return
+    return;
   }
   createPicture({
     title,
@@ -94,18 +103,18 @@ document.getElementById("read-all").addEventListener("click", () => {
 });
 
 //Read one
-document.getElementById('read-one').addEventListener("click", () => {
+document.getElementById("read-one").addEventListener("click", () => {
   const id = prompt("Introduce el id a buscar");
   readOne(id);
 });
 
 //Delete one
-document.getElementById('delete').addEventListener('click', () => {
+document.getElementById("delete").addEventListener("click", () => {
   deletePicture();
 });
 
 //Clean
-document.getElementById('clean').addEventListener('click', () => {
+document.getElementById("clean").addEventListener("click", () => {
   cleanAlbum();
 });
 
@@ -134,26 +143,26 @@ function readOne(id) {
   // Limpia el album para mostrar el resultado
   cleanAlbum();
 
-  //Petición a Firestore para leer un documento de la colección album 
+  //Petición a Firestore para leer un documento de la colección album
   var docRef = db.collection("album").doc(id);
 
-  docRef.get().then((doc) => {
-    if (doc.exists) {
-      console.log("Document data:", doc.data());
-      printPhoto(doc.data().title, doc.data().url, doc.id);
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
-  }).catch((error) => {
-    console.log("Error getting document:", error);
-  });
-
+  docRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+        printPhoto(doc.data().title, doc.data().url, doc.id);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
 }
 
 /**************Firebase Auth*****************/
-
-/*
 
 const signUpUser = (email, password) => {
   firebase
@@ -162,22 +171,23 @@ const signUpUser = (email, password) => {
     .then((userCredential) => {
       // Signed in
       let user = userCredential.user;
-      console.log(`se ha registrado ${user.email} ID:${user.uid}`)
-      alert(`se ha registrado ${user.email} ID:${user.uid}`)
+      console.log(`se ha registrado ${user.email} ID:${user.uid}`);
+      alert(`se ha registrado ${user.email} ID:${user.uid}`);
       // ...
       // Saves user in firestore
       createUser({
         id: user.uid,
         email: user.email,
-        message: "hola!"
+        message: "hola!",
       });
-
     })
     .catch((error) => {
-      console.log("Error en el sistema" + error.message, "Error: " + error.code);
+      console.log(
+        "Error en el sistema" + error.message,
+        "Error: " + error.code
+      );
     });
 };
-
 
 document.getElementById("form1").addEventListener("submit", function (event) {
   event.preventDefault();
@@ -186,43 +196,47 @@ document.getElementById("form1").addEventListener("submit", function (event) {
   let pass2 = event.target.elements.pass2.value;
 
   pass === pass2 ? signUpUser(email, pass) : alert("error password");
-})
-
+});
 
 const signInUser = (email, password) => {
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in
       let user = userCredential.user;
-      console.log(`se ha logado ${user.email} ID:${user.uid}`)
-      alert(`se ha logado ${user.email} ID:${user.uid}`)
+      console.log(`se ha logado ${user.email} ID:${user.uid}`);
+      alert(`se ha logado ${user.email} ID:${user.uid}`);
       console.log("USER", user);
     })
     .catch((error) => {
       let errorCode = error.code;
       let errorMessage = error.message;
-      console.log(errorCode)
-      console.log(errorMessage)
+      console.log(errorCode);
+      console.log(errorMessage);
     });
-}
+};
 
 const signOut = () => {
   let user = firebase.auth().currentUser;
 
-  firebase.auth().signOut().then(() => {
-    console.log("Sale del sistema: " + user.email)
-  }).catch((error) => {
-    console.log("hubo un error: " + error);
-  });
-}
-
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      console.log("Sale del sistema: " + user.email);
+    })
+    .catch((error) => {
+      console.log("hubo un error: " + error);
+    });
+};
 
 document.getElementById("form2").addEventListener("submit", function (event) {
   event.preventDefault();
   let email = event.target.elements.email2.value;
   let pass = event.target.elements.pass3.value;
-  signInUser(email, pass)
-})
+  signInUser(email, pass);
+});
 document.getElementById("salir").addEventListener("click", signOut);
 
 // Listener de usuario en el sistema
@@ -234,6 +248,3 @@ firebase.auth().onAuthStateChanged(function (user) {
     console.log("no hay usuarios en el sistema");
   }
 });
-
-*/
-
